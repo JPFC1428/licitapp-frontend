48e5b2ac9c6189492576ba636e4ea86c7619f92a
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-20 11:38:04 -0500

    Update procesosMock.js

--- src/data/procesosMock.js
+++ src/data/procesosMock.js
@@ -4,19 +4,29 @@ const procesos = [
     valor: '$970.000.000',
     objeto: 'Materiales de construcción',
     fechaCierre: '10 agosto 2024',
+    estado: 'convocatoria'
   },
   {
     entidad: 'MINISTERIO DE EDUCACIÓN',
     valor: '$1.200.000.000',
     objeto: 'Servicios de limpieza',
     fechaCierre: '15 agosto 2024',
+    estado: 'evaluación'
   },
   {
     entidad: 'GOBERNACIÓN DE ANTIOQUIA',
     valor: '$800.000.000',
     objeto: 'Proyectos viales',
     fechaCierre: '21 agosto 2024',
+    estado: 'cerrado'
   },
+  {
+    entidad: 'ALCALDÍA MUNICIPAL DE NEIVA',
+    valor: '$1.100.000.000',
+    objeto: 'Suministro de equipos',
+    fechaCierre: '30 agosto 2024',
+    estado: 'borrador'
+  }
 ];
 
 export default procesos;
