59e1ac2348b96b2df4adde357626c9663d30a13f
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-21 12:34:24 -0500

    Update ProcesoCard.jsx

--- src/components/ProcesoCard.jsx
+++ src/components/ProcesoCard.jsx
@@ -43,7 +43,7 @@ export default function ProcesoCard({ proceso }) {
 
       <button
         onClick={handleVerMas}
-        className='mt-3 bg-red-500 hover:bg-red-600 transition-colors text-white px-4 py-2 rounded'
+        className='mt-3 bg-blue-500 hover:bg-red-600 transition-colors text-white px-4 py-2 rounded'
       >
         Ver más
       </button>
