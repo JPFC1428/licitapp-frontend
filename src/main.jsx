ab2ccb8d37554423e296883c187d5a2efb8e9143
JPFC1428 <juapabfranco@utp.edu.co>
2025-06-21 00:20:09 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ProcesoDetalle from './components/ProcesoDetalle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/detalle/:id" element={<ProcesoDetalle />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
